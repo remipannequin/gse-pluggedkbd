0c26178ddb078eae09abb5939f84b2de54425852
Rémi Pannequin <remi.pannequin@univ-lorraine.fr>
2023-03-03 18:26:07 +0100

    edit keyboard associations

--- prefs.js
+++ prefs.js
@@ -2,10 +2,11 @@
 
 const {Adw, Gtk, Gio} = imports.gi;
 
+
 // It's common practice to keep GNOME API and JS imports in separate blocks
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
-
+const {PluggedKbdSettings} = Me.imports.pluggedKbd;
 
 /**
  * Like `extension.js` this is used for any one-time setup like translations.
@@ -14,31 +15,6 @@ const Me = ExtensionUtils.getCurrentExtension();
  */
 function init(meta) {
     log(`initializing ${meta.name} Preferences`);
-}
-
-
-/**
- * This function is called when the preferences window is first created to build
- * and return a GTK widget.
- *
- * As of GNOME 42, the preferences window will be a `Adw.PreferencesWindow`.
- * Intermediate `Adw.PreferencesPage` and `Adw.PreferencesGroup` widgets will be
- * used to wrap the returned widget if necessary.
- *
- * @returns {Gtk.Widget} the preferences widget
- */
-function buildPrefsWidget() {
-    // This could be any GtkWidget subclass, although usually you would choose
-    // something like a GtkGrid, GtkBox or GtkNotebook
-    const prefsWidget = new Gtk.Box();
-
-    // Add a widget to the group. This could be any GtkWidget subclass,
-    // although usually you would choose preferences rows such as AdwActionRow,
-    // AdwComboRow or AdwRevealerRow.
-    const label = new Gtk.Label({label: `${Me.metadata.name}`});
-    prefsWidget.append(label);
-
-    return prefsWidget;
 }
 
 /**
@@ -51,8 +27,7 @@ function buildPrefsWidget() {
  * @param {Adw.PreferencesWindow} window - The preferences window
  */
 function fillPreferencesWindow(window) {
-    const settings = ExtensionUtils.getSettings(
-        'org.gnome.shell.extensions.plugged-kbd');
+    const settings = new PluggedKbdSettings(ExtensionUtils);
 
     // Create a preferences page and group
     const page = new Adw.PreferencesPage();
@@ -66,11 +41,10 @@ function fillPreferencesWindow(window) {
 
     // Create the switch and bind its value to the `show-indicator` key
     const toggle = new Gtk.Switch({
-        active: settings.get_boolean('always-show-menuitem'),
+        active: settings.alwaysShowMenuitem,
         valign: Gtk.Align.CENTER,
     });
-    settings.bind(
-        'always-show-menuitem',
+    settings.bindAlwaysShowMenuitem(
         toggle,
         'active',
         Gio.SettingsBindFlags.DEFAULT
@@ -80,7 +54,119 @@ function fillPreferencesWindow(window) {
     row.add_suffix(toggle);
     row.activatable_widget = toggle;
 
-
     // Add our page to the window
     window.add(page);
+
+    // create another page for the rules
+    // Create a preferences page and group
+    const groupKbd = new KbdListGroup(settings, window);
+    page.add(groupKbd.group);
+}
+
+
+class KbdListGroup {
+    constructor(settings, parent) {
+        this._parent = parent;
+        this.group = new Adw.PreferencesGroup();
+        this.group.title = 'Keyboards';
+        this._settings = settings;
+        this._settings.connect('rules-changed', this.update.bind(this));
+        this._rows = [];
+        this._buttons = [];
+        this.update();
+    }
+
+    update() {
+        const rules = this._settings.rules;
+        rules.sort((a, b) => a[1] - b[1]);
+
+        for (const r of this._rows)
+            this.group.remove(r);
+        this._rows = [];
+        this._buttons = [];
+
+        for (const [kbdId, kbdPrio, kbdName, SrcId] of rules) {
+        // Create a preferences row for each rule
+            const kbdRow = new Adw.ActionRow();
+            kbdRow.title = kbdName;
+            kbdRow.subtitle = `${kbdId}, associated to ${SrcId} (priority ${kbdPrio})`;
+            const editButton = new Gtk.Button({icon_name: 'document-properties-symbolic', vexpand: false, valign: Gtk.Align.CENTER});
+            editButton.connect('clicked', this.showkbdDialog.bind(this, rules, kbdId));
+            kbdRow.add_suffix(editButton);
+            this.group.add(kbdRow);
+            this._rows.push(kbdRow);
+            this._buttons.push(editButton);
+        }
+    }
+
+
+    /**
+     * Display a dialog to edit keyboard display name and priority.
+     *
+     * @param {Array} rules current rules
+     * @param {str} kbdId id of the keyboard to edit
+     */
+    showkbdDialog(rules, kbdId) {
+        // get the current values
+        let kbdName;
+        let kbdPrio;
+        let index;
+        for (let i in rules) {
+            const [id, p, n, _] = rules[i];
+            if (id === kbdId) {
+                kbdName = n;
+                kbdPrio = p;
+                index = i;
+                break;
+            }
+        }
+        // transition to Adw.MessageDialog when Adw version >= 1.2
+        const modal = new Gtk.Dialog({
+            default_width: 400,
+            modal: true,
+            transient_for: this._parent,
+            title: kbdId,
+            use_header_bar: true,
+        });
+        const contentArea = modal.get_content_area();
+        const hb1 = new Gtk.Box({spacing: 10, margin_top: 10, margin_start: 10, margin_end: 10, homogeneous: true});
+        const hb2 = new Gtk.Box({spacing: 10, margin_top: 10, margin_bottom: 10, margin_start: 10, margin_end: 10, homogeneous: true});
+
+        const label = new Gtk.Label({label: 'Display Name', halign: Gtk.Align.START});
+        const edit = new Gtk.Entry({hexpand: true});
+        edit.buffer.text = kbdName;
+        hb1.append(label);
+        hb1.append(edit);
+
+        const label2 = new Gtk.Label({label: 'Priority', halign: Gtk.Align.START});
+        const prioSpin = new Gtk.SpinButton({hexpand: true});
+        prioSpin.adjustment.lower = 0;
+        prioSpin.adjustment.upper = rules.length - 1;
+        prioSpin.adjustment.step_increment = 1;
+        prioSpin.adjustment.value = kbdPrio;
+        hb2.append(label2);
+        hb2.append(prioSpin);
+
+        contentArea.append(hb1);
+        contentArea.append(hb2);
+
+        modal.add_button('Ok', Gtk.ResponseType.OK);
+        modal.add_button('Cancel', Gtk.ResponseType.CANCEL);
+        modal.connect('response', (_, rspId) => {
+            if (rspId === Gtk.ResponseType.OK) {
+                // update rules
+                rules[index][1] = prioSpin.value;
+                rules[index][2] = edit.buffer.text;
+                // set settings value
+                let toApply =  [];
+                for (let [i, p, n, s] of rules)
+                    toApply.push({kbdId: i, priority: p, kbdName: n, srcId: s});
+                this._settings.rules = toApply;
+            }
+            modal.destroy();
+        });
+
+
+        modal.show();
+    }
 }
